c413c2891e0860d7873a7d4bfc6f95c61b8b1622
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2022-04-04 14:39:15 +0700

    update authentication

--- components/authentication/authenticationController.js
+++ components/authentication/authenticationController.js
@@ -2,6 +2,9 @@ const async = require('hbs/lib/async');
 const service = require('../product/productService');
 
 
-exports.index = async(req, res, next) => {
+exports.showSignin = async(req, res, next) => {
     res.render('authentication/signin');
+}
+exports.showSignup = async(req, res, next) => {
+    res.render('authentication/signup');
 }
--- components/authentication/signupRoute.js
+++ components/authentication/signupRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller=require('./authenticationController');
 
 /* GET home page. */
-router.get('/', controller.showSignin);
+router.get('/', controller.showSignup);
 
 module.exports = router;
